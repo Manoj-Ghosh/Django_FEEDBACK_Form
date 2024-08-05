d24eb170657dceaf2ff28ee2500e6885e5443979
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-08-05 18:36:07 +0530

    new files created forms.py, and build in module forms used in views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,20 +1,33 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from .forms import ReviewForms 
+
 # Create your views here.
 
 def review(request):
     if request.method == "POST":
-        entered_username = request.POST['username']
-        if entered_username == "" and len(entered_username) >= 100:
-            return render(request, "reviews/review.html", {
-                "has_error": True
-            })
-        print(entered_username)
-        return HttpResponseRedirect("/thank-you")
+        #entered_username = request.POST['username']
+        form = ReviewForms(request.POST)
+
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank-you")
+            
+
+
+        # if entered_username == "" and len(entered_username) >= 100:
+        #     return render(request, "reviews/review.html", {
+        #         "has_error": True
+        #     })
+        # print(entered_username)
+        # return HttpResponseRedirect("/thank-you")
+
+    form = ReviewForms()
 
     return render(request, "reviews/review.html", {
-        "has_error": False
+        #"has_error": False
+        "form": form
     })
 
 

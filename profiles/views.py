2f925472bb1d41a0ed448573ca3c04d1479ccfe9
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-08-15 15:58:42 +0530

    changes happens in views.py to upload a image file and save it the temp folder

--- profiles/views.py
+++ profiles/views.py
@@ -5,11 +5,17 @@ from django.http import HttpResponseRedirect
 
 # Create your views here.
 
+def store_file(file):
+    with open("temp/image.jpeg", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
 
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        print(request.FILES["image"])
+        #print(request.FILES["image"])
+        store_file(request.FILES["image"])
         return HttpResponseRedirect("/profiles")

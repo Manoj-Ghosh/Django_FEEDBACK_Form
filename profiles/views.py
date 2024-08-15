ba3eff1cccaeced97265e024c75e807a5ad28138
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-08-15 19:46:47 +0530

    class CreateProfileView(CreateView) this class based createview is added in place of class CreateProfileView(View) in views.py file

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -14,25 +15,30 @@ from .models import UserProfile
 #         for chunk in file.chunks():
 #             dest.write(chunk)
 
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form" : form
-        })
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form" : form
+#         })
 
-    def post(self, request):
-        #print(request.FILES["image"])
-        submitted_form = ProfileForm(request.POST, request.FILES)
+#     def post(self, request):
+#         #print(request.FILES["image"])
+#         submitted_form = ProfileForm(request.POST, request.FILES)
 
-        if submitted_form.is_valid():
-            #store_file(request.FILES["image"])
-            profile = UserProfile(image = request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
+#         if submitted_form.is_valid():
+#             #store_file(request.FILES["image"])
+#             profile = UserProfile(image = request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
         
-        return render(request, "profiles/create_profile.html", {
-            "form" : submitted_form
-        })
+#         return render(request, "profiles/create_profile.html", {
+#             "form" : submitted_form
+#         })
         
